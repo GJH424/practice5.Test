2ffaddd09902a73e3c5433df9a26f592124faae9
mlrd424 <mlrd-@naver.com>
2022-06-19 12:46:58 +0900

    시험제출....

---
+++ RADEME.md
@@ -0,0 +1,8 @@
+# 안녕하세요! 학생점수관리 저장소입니다~
+본 저장소는 학생점수관리 test(220619)를 포함하고 있습니다.  
+사용한 기술 스택은 다음과 같습니다.
+
+- pure HTML, CSS, JS
+- jQuery (ajax)
+- python + flask
+- mysql
---
+++ main.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, jsonify, request
+import pymysql
+
+app = Flask(__name__)
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='web_test', charset="utf8")
+cursor = db.cursor()
+
+@app.route("/", methods=["GET"])
+def hello():
+	return "Hello Front-end"
+
+@app.route("/page", methods=["GET"])
+def mainPage():
+    return render_template('index.html')
+
+#GET API(db 불러오기)
+@app.route("/backend", methods=["GET"])
+def get_info():
+    cursor.execute("select * from student_score")
+    students = cursor.fetchall()
+    print(students)
+    info = []
+    for s in students:
+        info.append({
+            'id':s[0],
+            'name':s[1],
+            'kuk':s[2],
+            'eng':s[3],
+            'su':s[4]
+        })
+    return jsonify(info)
+
+#GET API(db 불러오기)
+@app.route("/backend/kuk", methods=["GET"])
+def kuk_sort():
+    cursor.execute("select * from student_score order by kuk desc")
+    students = cursor.fetchall()
+    # print(students)
+    info = []
+    for s in students:
+        info.append({
+            'id':s[0],
+            'name':s[1],
+            'kuk':s[2],
+            'eng':s[3],
+            'su':s[4]
+        })
+    return jsonify(info)
+
+#GET API(db 불러오기)
+@app.route("/backend/eng", methods=["GET"])
+def eng_sort():
+    cursor.execute("select * from student_score order by eng desc")
+    students = cursor.fetchall()
+    # print(students)
+    info = []
+    for s in students:
+        info.append({
+            'id':s[0],
+            'name':s[1],
+            'kuk':s[2],
+            'eng':s[3],
+            'su':s[4]
+        })
+    return jsonify(info)
+
+#GET API(db 불러오기)
+@app.route("/backend/su", methods=["GET"])
+def su_sort():
+    cursor.execute("select * from student_score order by su desc")
+    students = cursor.fetchall()
+    print(students)
+    info = []
+    for s in students:
+        info.append({
+            'id':s[0],
+            'name':s[1],
+            'kuk':s[2],
+            'eng':s[3],
+            'su':s[4]
+        })
+    return jsonify(info)
+
+
+#POST API(학생 저장)
+@app.route("/backend", methods=["POST"])
+def save():
+    name = request.form['name']
+    kuk = request.form['kuk']
+    su = request.form['su']
+    eng = request.form['eng']
+    cursor.execute(f"insert into student_score (name, kuk, eng, su) values ('{name}', {kuk}, {eng}, {su})")
+    db.commit()
+    return "POST API"
+
+#DELETE API(학생 삭제)
+@app.route("/backend", methods=["DELETE"])
+def delete():
+    name = request.args.get('name')
+    cursor.execute(f"delete from student_score where name='{name}'")
+    db.commit()
+    return "DELETE API"
+
+#PUT API(학생 수정)
+@app.route("/backend", methods=["PUT"])
+def put():
+    name = request.form['name']
+    kuk = request.form['kuk']
+    su = request.form['su']
+    eng = request.form['eng']
+    cursor.execute(f"update student_score set kuk={kuk}, su={su}, eng={eng} where name='{name}'")
+    db.commit()
+    return "PUT API"
+
+if __name__ == "__main__":
+    app.run(debug=True)
